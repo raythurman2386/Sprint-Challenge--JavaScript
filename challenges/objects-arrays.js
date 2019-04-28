d419ba8c9d0b43f251cde60d6c00e15e7233bf3d
Raymond Thurman <raymondthurman5@gmail.com>
2019-04-28 19:12:17 -0400

    dinosaur console logs complete

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -13,6 +13,9 @@ const tyrannosaurus = {
   weight: 7000,
   length: 12,
   period: 'Late Cretaceous',
+  speak: function () {
+    return `RAWERSRARARWERSARARARRRR!`;
+  }
 }
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
@@ -36,20 +39,20 @@ const velociraptor = {
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.name);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(`${stegosaurus.length} meters.`);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.speak());
 
 
 // ==== Arrays ====
